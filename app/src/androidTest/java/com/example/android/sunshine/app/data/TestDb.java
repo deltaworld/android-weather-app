edc01a87b24aa60e06c6a5adb9d019540d2a9ae6
Tareq Fadel <itrainit@gmail.com>
2015-07-19 15:11:01 +0100

    Refactor as Example

--- app/src/androidTest/java/com/example/android/sunshine/app/data/TestDb.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestDb.java
@@ -131,46 +131,49 @@ public class TestDb extends AndroidTestCase {
         // and our testLocationTable can only return void because it's a test.
 
         long locationRowId = insertLocation();
+
+        // Make sure we have a valid row ID.
+        assertFalse("Error: Location Not Inserted Correctly", locationRowId == -1L);
+
         // First step: Get reference to writable database
+        // If there's an error in those massive SQL table creation Strings
         WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        // Create ContentValues of what you want to insert
-        // (you can use the createWeatherValues TestUtilities function if you wish)
-        ContentValues testValues = TestUtilities.createWeatherValues(locationRowId);
+        // Second Step (Weather): Create weather values
+        ContentValues weatherValues = TestUtilities.createWeatherValues(locationRowId);
 
-        // Insert ContentValues into database and get a row ID back
-        long weatherRowId;
-        weatherRowId = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, testValues);
-
-        // Check that that the first row has been inserted correctly
+        // Third Step (Weather): Insert ContentValues into database and get a row ID back
+        long weatherRowId = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, weatherValues);
         assertTrue((weatherRowId != -1));
 
-        // Query the database and receive a Cursor back
-        Cursor cursor = db.query(
-                WeatherContract.WeatherEntry.TABLE_NAME, // Table to query
-                null, // all columns
-                null, // columns from the "where" clause
-                null, // values from the "where" clause
+        // Fourth Step Query the database and receive a Cursor back
+        // A cursor is your primary interface to the query results.
+        Cursor weatherCursor = db.query(
+                WeatherContract.WeatherEntry.TABLE_NAME, // Table to Query
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
                 null, // columns to group by
                 null, // columns to filter by row groups
                 null // sort order
         );
 
-        // Move the cursor to a valid database row
-        assertTrue("Error: No records returned from weather query", cursor.moveToFirst());
+        // Move the cursor to a valid database row and check to see if we have any rows
+        assertTrue("Error: No records returned from weather query", weatherCursor.moveToFirst());
 
-        // Validate data in resulting Cursor with the original ContentValues
-        // (you can use the validateCurrentRecord function in TestUtilities to validate the
-        // query if you like)
-        TestUtilities.validateCurrentRecord("Error: Weather query validation vailed",
-                cursor, testValues);
+        // Fifth Step: Validate the weather Query
+        TestUtilities.validateCurrentRecord("testInsertReadDb weatherEntry failed to validate",
+                weatherCursor, weatherValues);
 
-        // Finally, close the cursor and database
-        cursor.close();
+        // Move the cursor to demonstrate that there is only one record in the database
+        assertFalse("Error: More than one record returned from weather query",
+                weatherCursor.moveToNext());
+
+        // Sixth Step: Close the cursor and database
+        weatherCursor.close();
         db.close();
     }
-
 
     /*
         Students: This is a helper method for the testWeatherTable quiz. You can move your
@@ -228,7 +231,6 @@ public class TestDb extends AndroidTestCase {
         // 6: Finally, close the Cursor and Database
         cursor.close();
         db.close();
-
 
         return locationRowId;
     }
